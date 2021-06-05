9741533b1e7c02637be128d39ae335545d8df2f1
sivaram <sivaramk1411@gmail.com>
2021-06-05 17:17:13 +0530

    not create a object b'z abstarct method and class combination

--- RegulorDemo.py
+++ RegulorDemo.py
@@ -1,9 +1,15 @@
-import re
-p=input("Enter phone number")
-m=re.fullmatch("[7-9][0-9]{9}",p)
-if m!=None:
-    print("Valid number is ",p)
-else:
-    print("invalid number")
+from abc import *
+class Test(ABC):
+    @abstractmethod
+    def m1(self):
+       pass
+t=Test()
+    
+
+
+
+
+
+
 
 
