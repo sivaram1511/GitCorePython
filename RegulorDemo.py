4355b5c5386d12817a9f8c389e502cc295f00b7f
sivaram <sivaramk1411@gmail.com>
2021-06-15 17:46:52 +0530

    is and ==

--- RegulorDemo.py
+++ RegulorDemo.py
@@ -1,11 +1,8 @@
-def add(x):
-    result=1
-    while x>=1:
-        result=result*x
-        x=x-1
-    return result
-for i in range(1,6):
-    print(add(i))
+list1=["python","gopher"]
+list2=list1
+print(list1==list2)
+print(list1 is list2)
+
     
 
    
