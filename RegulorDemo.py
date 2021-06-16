e4febe7743c3c68800563b79a76a3c1109a58041
sivaram <sivaramk1411@gmail.com>
2021-06-16 17:26:00 +0530

    randrange

--- RegulorDemo.py
+++ RegulorDemo.py
@@ -1,9 +1,7 @@
-def f1():
-    if __name__=='__main__':
-        print("The code executed as a program")
-    else:
-        print("The code executed as a module")
-f1()        
+from random import*
+for i in range(10):
+    print(randrange(10,150,10))
+        
 
 
 
