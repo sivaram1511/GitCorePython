fbfabef0aff2239233ae146fea26b018bf93191b
sivaram <sivaramk1411@gmail.com>
2021-06-17 17:50:49 +0530

    Update RegulorDemo.py
    odd triangle

--- RegulorDemo.py
+++ RegulorDemo.py
@@ -1,7 +1,6 @@
-n=int(input("Enter range of pattern"))
+n=int(input("Enter range"))
 for i in range(1,n+1):
-    print(" "*(i-1),end='')
-    for j in range(1,n+2-i):
-        print(j,end='')
+    print(" "*(n-i),end='')
+    for j in range(1,2*i):
+        print("*",end=' ')
     print()    
-    
