1d44aa3f6b3526d79f020477f1735cd856e5888b
sivaram <sivaramk1411@gmail.com>
2021-06-17 16:54:24 +0530

    triamgle pattern

--- RegulorDemo.py
+++ RegulorDemo.py
@@ -1,26 +1,7 @@
-from random import*
-for i in range(10):
-    print(randrange(10,150,10))
-        
-
-
-
+n=int(input("Enter range of pattern"))
+for i in range(1,n+1):
+    print(" "*(i-1),end='')
+    for j in range(1,n+2-i):
+        print(j,end='')
+    print()    
     
-
-    
-
-
-
-    
-
-
-    
-
-   
-    
-
-
-
-
-
-
