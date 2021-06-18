3bcc5ffe56ad0390095b9f7a590b8e7d7b9d9b3e
sivaram <sivaramk1411@gmail.com>
2021-06-18 09:19:04 +0530

    format decimal(d)integer

--- RegulorDemo.py
+++ RegulorDemo.py
@@ -1,6 +1,3 @@
-name="sankar"
-salary=10000
-age=45
-print("{}'s salary is {} and his age {}".format(name,salary,age))
-
-   
+print("The Integer number is:{}".format(123))
+print("The integer number is:{:d}".format(123))
+print("The integer number is:{:5d}".format(123))
