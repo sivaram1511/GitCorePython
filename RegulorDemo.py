96821327d2901581c7a5f9c4fe73bba550c093bc
sivaram <sivaramk1411@gmail.com>
2021-06-29 21:40:42 +0530

    return the output to the caller

--- RegulorDemo.py
+++ RegulorDemo.py
@@ -1,21 +1,9 @@
-class Solution:
-     def countbinarysubstring(self,s):
-          prev,cur=0,1
-          output=0
-          n=len(s)
-          for i in range(1,n):
-               if s[i]!=s[i-1]:
-                    output+=min(cur,prev)
-                    prev=cur
-                    cur=1
-               else:
-                    cur+=1
-          print(output)          
-          return output+min(prev,cur)
-s='010100011101'
-s1=Solution()
-output=s1.countbinarysubstring(s)
-print(output)
+def add(a,b):
+    print(a,b)
+    return a+b
+sum=add(23,45)
+print(sum)
+
 
 
 
