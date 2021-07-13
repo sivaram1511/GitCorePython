a64f7aebcbb5f7881ce05c03c42626ee5f34564f
sivaram <sivaramk1411@gmail.com>
2021-07-13 21:06:10 +0530

    special way to from child class static method access the parent class constructor and instance method

--- RegulorDemo.py
+++ RegulorDemo.py
@@ -1,22 +1,32 @@
-d=dict([(100,'siva'),(200,'ram'),(300,'krishna')])
-print(d.values())
-for k,v in d.items():
-    print(k,"----",v)
-d1=d.copy()
-print(d1)
-d1[100]='kasi'
-print("original dict is",d)
-print("cloned copy",d1)
+class P:
+    def __init__(self):
+        print("parent class constructor")
+    def m1(self):
+        print("parent class M1 method")
+    @classmethod
+    def m2(cls):
+        print("parent cls m2 clas method")
+    @staticmethod
+    def m3():
+        print("parent class static method")
+class C(P):
+    @classmethod
+    def m1(cls):
+        super().m2()
+        super().m3()
+        super(C,cls).m1(cls)
+        super(C,cls).__init__(cls)
+C.m1()       
+        
 
-
-
-
-
-
-
-   
        
-         
+        
+        
+
+
+
+
+
 
 
 
