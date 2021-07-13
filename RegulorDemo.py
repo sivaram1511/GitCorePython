8683bbc0316eb14c2c60455a35dcb896d8c91130
sivaram <sivaramk1411@gmail.com>
2021-07-13 22:06:37 +0530

    magic method for operator overloading

--- RegulorDemo.py
+++ RegulorDemo.py
@@ -1,27 +1,12 @@
-class P:
-    def __init__(self):
-        print("parent class constructor")
-    def m1(self):
-        print("parent class M1 method")
-    @classmethod
-    def m2(cls):
-        print("parent cls m2 clas method")
-    @staticmethod
-    def m3():
-        print("parent class static method")
-class C(P):
-    @staticmethod
-    def m1():
-        super(C,C).m2()
-        super(C,C).m3()
-        super(C,C).m1(C)
-        super(C,C).__init__(C)
-C.m1()       
-        
+class Book:
+    def __init__(self,pages):
+        self.pages=pages
+    def __add__(self,others):
+        return self.pages+others.pages
+b1=Book(100)
+b2=Book(200)
+print(b1+b2)
 
-       
-        
-        
 
 
 
